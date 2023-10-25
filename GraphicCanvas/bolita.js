35c96742dd579daf1576d85c57f0efe888ff8a77
Marina <maryypucca@gmail.com>
2023-10-25 09:36:08 -0600

    10 Eventos Teclado Canvas JS

--- GraphicCanvas/bolita.js
+++ GraphicCanvas/bolita.js
@@ -3,8 +3,8 @@ const ctx = canvas.getContext("2d");
 let dirX = 1; 
 let dirY = 1; 
 
-let idX = 0;
-let idY = 0;
+let idx = 10;
+let idy = 19;
 
 let ini = 0;
 
@@ -16,34 +16,33 @@ function bolita(x,y){
  ctx.fillStyle = hslColor(ini);
  ini += 2;
  ctx.beginPath();
- ctx.arc(x,y,30,0,Math.PI * 2);
+ ctx.arc(x,y,25,0,Math.PI * 2);
  ctx.fill();
- ctx.stroke();
 
 }
 
 setInterval(() => {
-    ctx.clearRect(500,500,600,400);
-    bolita(idX,idY);
+   // ctx.clearRect(500,500,600,400);
+    bolita(idx,idy);
     if(dirX === 1 && dirY === 1){
-        idX += 50;
-        idY += 50;
+        idx += 50;
+        idy += 50;
     }
     else if (dirX === 1 && dirY === 2){
-        idX += 60;
-        idY -= 60;
+        idx += 60;
+        idy -= 60;
     }
     else if (dirX === 2 && dirY === 1){
-        idX -= 70;
-        idY += 20;
+        idx -= 70;
+        idy += 20;
     }else{
-        idX -= 20;
-        idY -= 20;
+        idx -= 20;
+        idy -= 20;
     }
 
     //cambiar direcciones
-    if(idX > 590) dirX = 2;
-    if(idX < 10) dirX = 1;
-    if(idY > 390) dirY = 2;
-    if(idY < 10) dirY = 1;
+    if(idx > 640) dirX = 2;
+    if(idy < 10) dirX = 1;
+    if(idx > 390) dirY = 2;
+    if(idy < 10) dirY = 1;
 },100);
